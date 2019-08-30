67dbade57185c269a0ec4316a819d422c6305497
Astiwiguna <54674646+Astiwiguna@users.noreply.github.com>
2019-08-30 16:19:27 +0700

    Delete Kredit.py

--- Kredit.py
+++
@@ -1,8 +0,0 @@
-print("Selamat datang di Link aja")
-k = int(input("\nMasukan jumlah uang yang ingin diambil:"))
-s = 1500000 
-x = s - k 
-if (s > k): 
-    print("Saldo anda tersisa:", x)
-else :
-    print("\nSaldo anda tidak mencukupi")
